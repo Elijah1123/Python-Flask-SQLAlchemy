9d3f55a6476f661f875018d48c8c43f9df1f9bf8
Elijah1123 <elijahmzalendo659@gmail.com>
2025-11-26 12:54:02 +0300

    flask-sqlalchemy

---
+++ server/app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from flask import Flask, make_response, jsonify
+from flask_migrate import Migrate
+
+from models import db, Earthquake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:newpassword@localhost:5432/app_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return make_response({'message': 'Flask SQLAlchemy Lab 1'}, 200)
+
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    quake = Earthquake.query.filter_by(id=id).first()
+    if quake:
+        return make_response(quake.to_dict(), 200)
+    return make_response({'error': 'Earthquake not found'}, 404)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    quake_list = [q.to_dict() for q in quakes]
+    return make_response({
+        "count": len(quake_list),
+        "quake": quake_list
+    }, 200)
+        
+
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
